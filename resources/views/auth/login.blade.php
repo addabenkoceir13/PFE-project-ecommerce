8aa614f3d78ad45d74cdeeaebbf069bb18c14188
Adda Benkoceir Mohamed <64089992+addabenkoceir13@users.noreply.github.com>
2023-05-15 23:36:24 +0100

    update 05152023

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<section class="d-flex justify-content-center align-items-center vh-100">
+        <div class="container-fluid h-custom">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-md-9 col-lg-6 col-xl-5">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                    class="img-fluid" alt="Sample image">
+                </div>
+                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-center mb-5">
+                            <h2 class="lead fw-normal mb-0 me-2">Sign in </h2>
+                        </div>
+                        <!-- Email input -->
+                        <div class="form-outline form-floating mb-4">
+                            <input type="email"  class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required placeholder="Enter a valid email address" />
+                            <label class="form-label" for="form3Example3">Email address</label>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <!-- Password input -->
+                        <div class="form-outline form-floating mb-3">
+                            <input type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter password" />
+                            <label class="form-label" for="form3Example4">Password</label>
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="d-flex justify-content-between align-items-center">
+                            <!-- Checkbox -->
+                            <div class="form-check mb-0">
+                                <input class="form-check-input me-2" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} />
+                                <label class="form-check-label" for="remember">
+                                    Remember me
+                                </label>
+                            </div>
+                            @if (Route::has('password.request'))
+                            <a class="text-body" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                            @endif
+                            {{-- <a href="#!" class="text-body">Forgot password?</a> --}}
+                        </div>
+
+                        <div class="text-center text-lg-start mt-4 pt-2">
+                            <button type="submit" class="btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="{{ route('register') }}"
+                                class="link-danger">Register</a></p>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+</section>
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +133,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
